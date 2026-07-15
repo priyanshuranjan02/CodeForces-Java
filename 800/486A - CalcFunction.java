4ab77b015719a2e092ce7cc850c47b00a2ecbc2a
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-07-15 10:07:35 +0530

    Add files via upload

---
+++ 800/486A - CalcFunction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class CalcFunction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+
+        if(n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n + 1) / 2);
+        }
+    }
+}
