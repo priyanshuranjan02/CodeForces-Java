845e936b572d1a0e97087344599a63106907a51e
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-07-17 11:13:42 +0530

    Add files via upload

---
+++ 800/630A - AgainTwentyFive.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.next();
+
+        System.out.println(25);
+    }
+}
