4394a4040bdcf12566ec50f7dbf762016e746923
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-07-18 10:52:54 +0530

    Add files via upload

---
+++ 800/791A - BearAndBrother.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class BearAndBrother {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int years = 0;
+
+        while(a <= b) {
+            a *= 3;
+            b *= 2;
+            years++;
+        }
+        System.out.println(years);
+    }
+}
