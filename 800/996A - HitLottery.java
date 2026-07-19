9cbdf90ed87c2cc453599bf98df671d3866be6bc
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-07-19 11:43:02 +0530

    Add files via upload

---
+++ 800/996A - HitLottery.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class HitLottery {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] notes = {100, 20, 10, 5, 1};
+        int count = 0;
+
+        for (int note : notes) {
+            count += n / note;
+            n %= note;
+        }
+
+        System.out.println(count);
+    }
+}
