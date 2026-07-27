b524150aaf4dda5e35308847cf5597f50a422e62
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-07-27 23:31:47 +0530

    Add files via upload

---
+++ 900/2140B - AnotherDivisible.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class AnotherDivisible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            long x = sc.nextLong();
+            System.out.println(2 * x);
+        }
+    }
+}
