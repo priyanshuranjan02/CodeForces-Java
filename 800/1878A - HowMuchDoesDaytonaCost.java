ffc98b890f13141fd7313155ef2b03b6fa7ad370
Priyanshu Ranjan <156450236+priyanshuranjan02@users.noreply.github.com>
2026-09-02 12:18:38 +0530

    Add files via upload

---
+++ 800/1878A - HowMuchDoesDaytonaCost.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class HowMuchDoesDaytonaCost {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+
+            boolean found = false;
+
+            for (int i = 0; i < n; i++) {
+                int x = sc.nextInt();
+
+                if (x == k) {
+                    found = true;
+                }
+            }
+            System.out.println(found ? "YES" : "NO");
+        }
+    }
+}
